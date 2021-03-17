91d6eb6ed83611ba5899233ecd3c943e8d7ebb90
Rafael Reis <rafael.araujoreis@hotmail.com>
2021-03-17 07:57:20 -0300

    Casos de Estudo

---
+++ script/landing-page.js
@@ -0,0 +1,8 @@
+function imgSlider(image) {
+    document.querySelector('.coffee').src = image;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
---
+++ script/netflix.js
@@ -0,0 +1,20 @@
+function toggleVideo() {
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+
+    video.currentTime = 0;
+    trailer.classList.toggle('active');
+
+    const active = document.querySelector('.active')
+    if (active != null) {
+        video.play();
+    } else {
+        video.pause();
+    }
+
+
+
+
+
+
+}
